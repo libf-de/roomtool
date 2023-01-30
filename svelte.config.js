2a2f0e1a42f6289796af49bb04626d88bb644024
Fabian Schillig <xorgmc@gmail.com>
2023-01-31 00:40:08 +0100

    Fixed docker build?

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +7,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapterNode()
 	}
 };
 
